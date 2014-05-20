d0481c44a66b0bcb8c9555fa17f19e3731ebf8bf
Chris Lush <chris.lush@mobilefun.co.uk>
2014-05-20 10:51:38 +0100

    Fix a defence range bug, add some doc comments and refactor

--- app/scripts/warriors-test.js
+++ app/scripts/warriors-test.js
@@ -1,3 +1,7 @@
+/**
+ * Helper class for various utilities
+ * @type {Object}
+ */
 var Utils = {
     getRandomArbitrary: function(min, max) {
         return Math.random() * (max - min) + min;
@@ -7,8 +11,20 @@ var Utils = {
     }
 }
 
+/**
+ * Base warrior class. Has the following attributes:
+ * name, health, attack, defense, speed, evade chance
+ * @return {Object} An instance of Warrior
+ */
 var Warrior = (function () {
 
+    /**
+     * Constructor that assigns the warrior's attributes.
+     * The values for each attribute are specified as a range,
+     * on creation each warrior will be assigned a random value
+     * within this range for the given attribute.
+     * @param {Object} props Attributes for the warrior
+     */
     function Warrior(props) {
         // assign default values
         var name     = props.name         || "Warrior",
@@ -18,34 +34,47 @@ var Warrior = (function () {
         speedRange   = props.speedRange   || [0,100],
         evadeRange   = props.evadeRange   || [0,1];
 
-        this.name       = name;
+        this.name        = name;
         // calculate attributes randomly based on ranges
-        this.maxHealth  = Utils.getRandomInt(healthRange[0], healthRange[1]);
-        this.attack     = Utils.getRandomInt(attackRange[0], attackRange[1]);
-        this.defence    = Utils.getRandomInt(defenceRange[0], attackRange[1]);
-        this.speed      = Utils.getRandomInt(speedRange[0], speedRange[1]);
-        this.evade      = Utils.getRandomArbitrary(evadeRange[0], evadeRange[1]);
+        this.maxHealth   = Utils.getRandomInt(healthRange[0], healthRange[1]);
+        this.attack      = Utils.getRandomInt(attackRange[0], attackRange[1]);
+        this.defence     = Utils.getRandomInt(defenceRange[0], defenceRange[1]);
+        this.speed       = Utils.getRandomInt(speedRange[0], speedRange[1]);
+        this.evadeChance = Utils.getRandomArbitrary(evadeRange[0], evadeRange[1]);
         // keep track of the warrior's current health
-        this.health     = this.maxHealth;
+        this.health      = this.maxHealth;
     }
 
     Warrior.prototype.constructor = Warrior;
 
+    /**
+     * Returns the possible damage the warrior can do in a single attack
+     * @return {Number} A damage value
+     */
     Warrior.prototype.doAttack = function () {
         return this.attack;
     }
 
+    /**
+     * Calculates whether the warrior will successfully evade an attack
+     * @return {Boolean} True if the warrior can evade, false otherwise
+     */
     Warrior.prototype.doEvade = function () {
-        return (Math.random() < this.evade);
+        return (Math.random() < this.evadeChance);
     }
 
+    /**
+     * Get attacked by another warrior. Checks certain conditions before applying
+     * the calculated damage to self
+     * @param  {Object} attacker  The Warrior that is attacking this instance
+     * @return {Boolean}          True if the attack was successful, false otherwise
+     */
     Warrior.prototype.getAttackedBy = function (attacker) {
-
         var self = this.constructor.name,
             foe = attacker.constructor.name,
             damage = 0;
 
-        // check if we will evade the attack
+        // check if the attack will be evaded
         if (this.doEvade()) {
             console.log(self + " avoided " + foe + "'s attack!");
             return false;
@@ -54,32 +83,41 @@ var Warrior = (function () {
         // calculate the damage to be taken
         damage = (attacker.doAttack() - this.defence);
 
-        // make sure we're not receiving negative damage
+        // make sure negative damage won't be applied
         if (damage <= 0) {
             console.log(foe + "'s attack connected but didn't even scratch " + self + "!");
             return false;
         }
 
-        // we were successfully attacked :(
+        // apply the damage
         this.health -= damage;
         console.log(foe + " hit " + self + " for " + damage + " damage");
 
+        // successful hit
         return true;
     }
 
+    /**
+     * @return {String} A character sheet for the warrior
+     * @override
+     */
     Warrior.prototype.toString = function () {
-      return this.name + "'s character sheet: " +
-      " Max Health: " + this.maxHealth +
+      return this.constructor.name + "'s character sheet: " +
       " Current Health: " + this.health +
+      ", Max Health: " + this.maxHealth +
       ", Attack: " + this.attack +
       ", Defence: " + this.defence +
       ", Speed: " + this.speed +
-      ", Evade: " + this.evade;
+      ", Evade Chance: " + this.evadeChance;
     }
 
     return Warrior;
 })();
 
+/**
+ * Ninja class, extends Warrior with class-specific attributes
+ * @return {Object} An instance of Ninja
+ */
 var Ninja = (function () {
 
     var my = {
@@ -88,7 +126,7 @@ var Ninja = (function () {
         attackRange:  [60,70],
         defenceRange: [20,30],
         speedRange:   [90,100],
-        evadeRange:   [0.3,0.5],
+        evadeRange:   [0.30,0.50],
         special: {
             attackModifier: 2,
             chance: 0.05
@@ -102,11 +140,17 @@ var Ninja = (function () {
     Ninja.prototype = Object.create(Warrior.prototype);
     Ninja.prototype.constructor = Ninja;
 
+    /**
+     * Each attack has a chance of triggering the Ninja's special attribute,
+     * a 5% chance of doubling attack strength.
+     * Calls the parent method to get the attack value after it has been modified
+     * @override
+     */
     Ninja.prototype.doAttack = function () {
-        // 5% chance of doubling attack strength
         if (Math.random() < my.special.chance) {
+            // calculate special
             this.attack *= my.special.attackModifier;
-            console.log("Ninja has doubled their attack strength!");
+            console.log(this.constructor.name + " has increased their attack strength!");
         }
 
         return Warrior.prototype.doAttack.call(this);
@@ -115,6 +159,10 @@ var Ninja = (function () {
     return Ninja;
 })();
 
+/**
+ * Samurai class, extends Warrior with class-specific attributes
+ * @return {Object} An instance of Samauri
+ */
 var Samurai = (function () {
 
     var my = {
@@ -123,10 +171,10 @@ var Samurai = (function () {
         attackRange:  [75,80],
         defenceRange: [35,40],
         speedRange:   [60,80],
-        evadeRange:   [0.3,0.4],
+        evadeRange:   [0.30,0.40],
         special: {
             regenAmount: 10,
-            chance: 0.50
+            chance: 0.10
         }
     };
 
@@ -137,21 +185,32 @@ var Samurai = (function () {
     Samurai.prototype = Object.create(Warrior.prototype);
     Samurai.prototype.constructor = Samurai;
 
+    /**
+     * The Samauri has a 10% chance of regaining 10 health on a successful evade,
+     * but regain more health points than their maximum health value.
+     * Calls the parent method to check whether the attack was evaded before calculating the special
+     * @override
+     */
     Samurai.prototype.doEvade = function () {
-        var success = Warrior.prototype.doEvade.call(this);
+        var evaded = Warrior.prototype.doEvade.call(this);
 
-        // chance to regen health on a successful evade
-        if (success && (Math.random() < my.special.chance)) {
-            this.health += my.special.regenAmount;
-            console.log("Samurai regained health!");
+        if (evaded && (Math.random() < my.special.chance)) {
+            // calculate special
+            var regainedHealth = (this.health + my.special.regenAmount);
+            this.health = (regainedHealth < this.maxHealth ? regainedHealth : this.maxHealth);
+            console.log(this.constructor.name + " regained health!");
         }
 
-        return success;
+        return evaded;
     }
 
     return Samurai;
 })();
 
+/**
+ * Brawler class, extends Warrior with class-specific attributes
+ * @return {Object} An instance of Brawler
+ */
 var Brawler = (function () {
 
     var my = {
@@ -160,9 +219,10 @@ var Brawler = (function () {
         attackRange:  [65,75],
         defenceRange: [40,50],
         speedRange:   [40,65],
-        evadeRange:   [0.3,0.35],
+        evadeRange:   [0.30,0.35],
         special: {
             bonusDefence: 10,
+            threshold: 0.20,
             applied: false
         }
     };
@@ -174,18 +234,23 @@ var Brawler = (function () {
     Brawler.prototype = Object.create(Warrior.prototype);
     Brawler.prototype.constructor = Brawler;
 
+    /**
+     * The Brawler increases their defence by 10 when their health falls below a threshold of
+     * 20% of their maximum health points.
+     * Calls the parent method to apply damage from the attacker before calculating the special
+     * @override
+     */
     Brawler.prototype.getAttackedBy = function (attacker) {
-        // attack as usual
-        var success = Warrior.prototype.getAttackedBy.call(this, attacker);
+        var wasHit = Warrior.prototype.getAttackedBy.call(this, attacker);
 
-        // increase defence if health drops below a certain amount
-        if (!my.special.applied && (this.health / this.maxHealth) < 0.2) {
+        if (!my.special.applied && (this.health / this.maxHealth) < my.special.threshold) {
+            // calculate special
             this.defence += my.special.bonusDefence;
             my.special.applied = true;
-            console.log("Brawler has increased their defence!");
+            console.log(this.constructor.name + " has increased their defence!");
         }
 
-        return success;
+        return wasHit;
     }
 
     return Brawler;
@@ -195,10 +260,14 @@ var n = new Ninja();
 var s = new Samurai();
 var b = new Brawler();
 
-//console.log(n.toString());
-//console.log(s.toString());
-//console.log(b.toString());
-
-//console.log(n.doAttack());
 s.getAttackedBy(b);
+s.getAttackedBy(n);
+console.log(s.toString());
+
+b.getAttackedBy(s);
+b.getAttackedBy(n);
 console.log(b.toString());
+
+n.getAttackedBy(s);
+n.getAttackedBy(b);
+console.log(n.toString());
